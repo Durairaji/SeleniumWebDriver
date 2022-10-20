6f1b5b7f2be611e9b3042d33c776171f5abc6d9f
Durairaji <112107468+Durairaji@users.noreply.github.com>
2022-10-20 07:16:40 -0700

    screenshot

---
+++ Screenshort.java
@@ -0,0 +1,21 @@
+package alert;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Screenshort {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\DURAIRAJ\\eclipse-workspace\\lib\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+	    driver.navigate().to("");
+		
+	
+	
+	
+	
+	
+	
+	}	
+}
