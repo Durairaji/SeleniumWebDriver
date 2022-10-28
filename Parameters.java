0a6a877f34a8ea550eb5c38158102f427a59ad68
Durairaji <112107468+Durairaji@users.noreply.github.com>
2022-10-28 08:40:42 -0700

    multiple cross browser test

--- Parameters.java
+++ Parameters.java
@@ -7,21 +7,35 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+
 public class Parameters {
-	@org.testng.annotations.Parameters({"email","pass"})
-@Test
-	public void logIn(String email,String pass) {
-		System.out.println("emaillocator"+email+"passlocator"+pass);
+	RemoteWebDriver driver;
+
+	@org.testng.annotations.Parameters({ "email", "pass", "browser" })
+	@Test
+	public void logIn(String email, String pass, String browser) {
+
+		System.out.println("emaillocator" + email + "passlocator" + pass);
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\DURAIRAJ\\eclipse-workspace\\lib\\driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		switch (browser) {
+		case "chrome":
+			break;
+		case "firefox":
+			break;
+		default:
+			System.out.println("browser is not defined");
+			break;
+
+		}
 		driver.get("https://www.facebook.com/login/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		WebElement findElement = driver.findElement(By.id("email"));
 		findElement.sendKeys(email);
 		WebElement findElement2 = driver.findElement(By.id("pass"));
-		findElement2.sendKeys(pass,Keys.TAB,Keys.TAB,Keys.ENTER);
+		findElement2.sendKeys(pass, Keys.TAB, Keys.TAB, Keys.ENTER);
 		String title = driver.getTitle();
 		System.out.println(title);
 		driver.quit();
